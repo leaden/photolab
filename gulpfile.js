72478eb83220ce0a94a3b9ca044f8ecb17d7269c
maribel <maribel.diaz.j@gmail.com>
2017-07-28 11:08:41 -0400

    creando estilos sass

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,12 @@ gulp.task('style', function(){
 });
 
 gulp.task('webserver', function(){
-	gulp.src('../gulpie/')
+	gulp.src('../photolab/')
 	.pipe(webserver({
 		fallback: 'index.html',
 		livereload: true,
 		directoryListing: false,
-		open: true
+		open: true,
 	}));
 });
 
